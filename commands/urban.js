c5fba04e77594733666b4512e69e17268daa760f
harshhh-dev <harshhh.singh.personal@gmail.com>
2020-09-08 22:54:54 -0400

    Yet another project on my plate :sweat:

---
+++ commands/urban.js
@@ -0,0 +1,29 @@
+const urban = require('urban');
+
+const { Client, Collection, MessageEmbed} = require('discord.js')
+
+module.exports.config = { 
+    name: "urban",
+    aliases: []
+}
+
+module.exports.run = async (client, message, args) => {
+   if (args.length < 1) return message.channel.send('Please enter a word');
+   
+   let word = args.join(' ');
+   console.log(word);
+
+   urban(word).first(json => {
+      if (!json) return message.channel.send('No such word exist!');
+
+      console.log(json);
+      const def = new MessageEmbed()
+          .setTitle(json.word)
+          .setDescription(json.definition)
+          .addField('Upvotes', json.thumbs_up, true)
+          .addField('Downvotes', json.thumb_down, true)
+          .setTimestamp(new Date())
+
+      message.channel.send(def);
+   })
+}
